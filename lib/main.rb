aa8d1324258fc5be0881bd1bde8481d23a9d2c91
Dane Dalton <danezdalton1@gmail.com>
2022-08-01 12:15:08 -0500

    #move_set creates all possibilities for the next 6 moves

--- lib/main.rb
+++ lib/main.rb
@@ -4,13 +4,40 @@ class Square
 
   def initialize(position)
     @position = position
-    @possible_moves = []
+    @moves = []
   end
 end
 
+class KnightGraph
 
+  attr_accessor :graph
 
-def knight_moves(start, end)
+  @@ALL_MOVES = [[2, -1], [2, 1], [1, 2], [1, -2],[-1, 2], [-1, -2], [-2, 1], [-2, -1]]
+
+  def initialize(start, finish)
+    @graph = move_set(start, finish)
+  end
+
+  def move_set(start, finish, counter = 6)
+    return p "Found" if start == finish
+    return p "Error" if counter == 0
+    square = Square.new(start)
+    @@ALL_MOVES.each do |move|
+      add_x = move[0] + start[0]
+      add_y = move[1] + start[1]
+      if (add_x >= 0 && add_x < 8 && add_y >= 0 && add_y < 8)
+        square.moves << [add_x, add_y]
+      end
+    end
+    counter -= 1
+    square.moves.map do |move|
+      move = move_set(move, finish, counter)
+    end
+    return square
+  end
+end
+
+def knight_moves(start, finish)
   #tree of all possible moves
   ##knight moves in an L shape
   ##cant move off the board
@@ -21,3 +48,6 @@ def knight_moves(start, end)
 
 
 end
+
+knight_graph = KnightGraph.new([1, 1], [3, 2])
+p knight_graph.graph
