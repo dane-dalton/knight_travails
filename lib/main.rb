49d6aa7089260d5c6140a32c54b160bdb56cd1ea
Dane Dalton <danezdalton1@gmail.com>
2022-08-18 16:13:49 -0400

    Change square_a and square_b for the knights start and end points. knight_moves writes out the sequence the knight will travel, and the minimum number of moves it takes to make it to the end.

--- lib/main.rb
+++ lib/main.rb
@@ -11,37 +11,36 @@ end
 
 class KnightGraph
 
-  attr_accessor :graph, :finish
+  attr_accessor :graph, :start , :finish
 
   @@ALL_MOVES = [[2, -1], [2, 1], [1, 2], [1, -2],[-1, 2], [-1, -2], [-2, 1], [-2, -1]]
 
   def initialize(start, finish)
-    @graph = move_set(start, finish)
+    @graph = move_set(start)
+    @start = start
     @finish = finish
   end
 
-  def move_set(start, finish, counter = 6)
-    return Square.new(start) if start == finish
-    return Square.new(start) if counter == 0
+  def move_set(start_move, counter = 6)
+    return Square.new(start_move) if start_move == self.finish
+    return Square.new(start_move) if counter == 0
     counter -= 1
-    square = Square.new(start)
+    square = Square.new(start_move)
     valid_moves = []
     @@ALL_MOVES.each do |move|
-      add_x = move[0] + start[0]
-      add_y = move[1] + start[1]
+      add_x = move[0] + start_move[0]
+      add_y = move[1] + start_move[1]
       if (add_x >= 0 && add_x < 8 && add_y >= 0 && add_y < 8)
         valid_moves << [add_x, add_y]
       end
     end
 
     valid_moves.each do |move|
-      square.moves << move_set(move, finish, counter)
+      square.moves << move_set(move, counter)
     end
-    # p square
+    
     unless square.moves[0].is_a?(Array)
       square.moves.each do |move|
-        # p move
-        puts "mommy"
         move.parent = square
       end
     end
@@ -54,26 +53,35 @@ class KnightGraph
 
     queue.each do |square|
       square.moves.each do |move|
-        return move.position if move.position == self.finish
+        return move if move.position == self.finish
         queue << move
       end
     end
 
     return nil
   end
+
+  def sequence
+    fast_sequence = []
+    fast_sequence << self.bfs
+
+    fast_sequence.each do |move|
+      fast_sequence << move.parent unless move.parent == nil
+    end
+
+    puts "Find the moves for a knight to get from square #{self.start} to square #{self.finish}: "
+    fast_sequence.reverse_each { |move| puts "#{move.position}" }
+    puts "The fastest sequence takes #{fast_sequence.length - 1} moves."
+  end
 end
 
 def knight_moves(start, finish)
-  #tree of all possible moves
-  ##knight moves in an L shape
-  ##cant move off the board
-  ##dont move to a position that has been visited
-
-  #perform a BFS and pick the first end move scenario
-  #perform a DFS traversal to print each move taken to that move, and the number of moves
-
-
+  knight_graph = KnightGraph.new(start, finish)
+  knight_graph.sequence
 end
 
-knight_graph = KnightGraph.new([7, 0], [0, 7])
-p knight_graph.bfs
+square_a = [0, 1]
+
+square_b = [6, 7]
+
+knight_moves(square_a, square_b)
