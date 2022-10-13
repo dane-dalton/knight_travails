9148aeb8119b12cbb2a72e5bc12c2ec954025df7
Dane Dalton <danezdalton1@gmail.com>
2022-10-13 13:01:02 -0500

    One more comment for #move_set

--- lib/knight_graph.rb
+++ lib/knight_graph.rb
@@ -18,26 +18,7 @@ class KnightGraph
 
   private
 
-    def sequence
-      knight_sequence = []
-      knight_sequence << self.bfs
-      trace_last_move_to_first_move(knight_sequence)
-      print_sequence(knight_sequence)
-    end
-
-    def trace_last_move_to_first_move(knight_sequence)
-      knight_sequence.each do |move|
-        knight_sequence << move.parent unless move.parent == nil
-      end
-    end
-
-    def print_sequence(knight_sequence)
-      puts "Find the moves for a knight to get from square #{self.start} to square #{self.finish}: "
-      knight_sequence.reverse_each { |move| puts "#{move.position}" }
-      puts "The fastest sequence takes #{knight_sequence.length - 1} moves."
-    end
-
-    #Most number of moves needed is 6. It is temporally expensive to search more moves than this.
+    #Finds every valid move a knight could make, and creates a new node(Square) for each move. Most number of moves needed is 6. It is temporally expensive to search more moves than this.
     def move_set(start_move, counter = 6)
       return Square.new(start_move) if (start_move == self.finish || counter == 0)
       counter -= 1
@@ -69,6 +50,25 @@ class KnightGraph
       square.moves.each do |move|
         move.parent = square
       end
+    end
+
+    def sequence
+      knight_sequence = []
+      knight_sequence << self.bfs
+      trace_last_move_to_first_move(knight_sequence)
+      print_sequence(knight_sequence)
+    end
+
+    def trace_last_move_to_first_move(knight_sequence)
+      knight_sequence.each do |move|
+        knight_sequence << move.parent unless move.parent == nil
+      end
+    end
+
+    def print_sequence(knight_sequence)
+      puts "Find the moves for a knight to get from square #{self.start} to square #{self.finish}: "
+      knight_sequence.reverse_each { |move| puts "#{move.position}" }
+      puts "The fastest sequence takes #{knight_sequence.length - 1} moves."
     end
 
     def bfs 
