a770b66ec6e12675fcf02575b5745361fdb3b34d
Dane Dalton <danezdalton1@gmail.com>
2022-10-14 11:17:23 -0500

    Refactor so the graph is built out as the bfs is happening. Make board size a variable to easily change it if necessary

--- lib/main.rb
+++ lib/main.rb
@@ -1,9 +1,50 @@
-require_relative 'knight_graph.rb'
+require_relative 'square.rb'
 
-def knight_moves(starting_square, ending_square)
-  knight_graph = KnightGraph.new(starting_square, ending_square)
+class RunMoves
+
+  attr_reader :starting_square, :ending_square
+
+  attr_accessor :knight_square, :queue
+
+  def initialize(starting_square, ending_square)
+    @starting_square = starting_square
+    @ending_square = ending_square
+    @knight_square = BoardSquare.new(starting_square)
+    @queue = []
+  end
+
+  def knight_moves
+    knight_bfs()
+    print_sequence(create_knight_history())
+  end
+
+  private
+    def knight_bfs
+      until @knight_square.position == @ending_square
+        @knight_square.get_moves.each do |move|
+          queue << BoardSquare.new(move, @knight_square)
+        end
+        @knight_square = @queue.shift #ruby's version of dequeue
+      end
+    end
+
+    def create_knight_history
+      knight_history = []
+      until @knight_square.parent == nil
+        knight_history << @knight_square.position
+        @knight_square = @knight_square.parent
+      end
+      return knight_history << @knight_square.position #add the last node that has no parent
+    end
+
+    def print_sequence(knight_history)
+      puts "Find the moves for a knight to get from square #{@starting_square} to square #{@ending_square}: "
+      knight_history.reverse_each { |move| puts "#{move}" }
+      puts "The fastest sequence takes #{knight_history.length - 1} moves."
+    end
 end
 
-starting_square = [7, 6]
-ending_square = [1, 3]
-knight_moves(starting_square, ending_square)
+starting_square = [10, 9]
+ending_square = [1, 1]
+x = RunMoves.new(starting_square, ending_square)
+x.knight_moves
--- lib/square.rb
+++ lib/square.rb
@@ -1,12 +1,22 @@
 class BoardSquare
 
-  attr_reader :position
+  attr_reader :position, :parent
 
-  attr_accessor :moves, :parent
+  ALL_MOVES = [[2, -1], [2, 1], [1, 2], [1, -2], [-1, 2], [-1, -2], [-2, 1], [-2, -1]]
+  BOARD_SIZE = [1, 8]
 
-  def initialize(position)
+  def initialize(position, parent = nil)
     @position = position
-    @moves = []
-    @parent = nil
+    @parent = parent
   end
+
+  def get_moves
+    ALL_MOVES.map { |move| [move[0] + @position[0], move[1] + @position[1]] }.keep_if { |move| valid_move?(move) }
+  end
+
+  private
+
+    def valid_move?(move)
+      move[0].between?(BOARD_SIZE[0], BOARD_SIZE[1]) && move[1].between?(BOARD_SIZE[0], BOARD_SIZE[1])
+    end
 end
